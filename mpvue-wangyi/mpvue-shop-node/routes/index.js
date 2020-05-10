731b8dddb4ba437f7f83bf94e4b9667b2ec71d5f
huangxiaoxin-hxx <1228620998@qq.com>
2020-05-10 18:22:24 +0800

    搭建后台，连接mysql，建立router,controllers

---
+++ mpvue-wangyi/mpvue-shop-node/routes/index.js
@@ -0,0 +1,11 @@
+const router = require('koa-router')({
+  prefix: '/wy'
+})
+
+const controllers = require('../controllers')
+
+router.get('/index/index', controllers.home.index)
+
+
+
+module.exports = router
